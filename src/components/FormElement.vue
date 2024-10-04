fe4333c8dbe21a63d004e336ccb588983034e5b4
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2024-10-04 16:15:29 +0900

    remove old pics and add WeatherBlock component

--- src/components/FormElement.vue
+++ src/components/FormElement.vue
@@ -1,4 +1,6 @@
 <script setup lang="js">
+import WeatherBlock from "./WeatherBlock.vue"
+
 defineProps({
   coordinates: {
     type: Object,
@@ -51,23 +53,12 @@ const afterTitles = ["°С", "%"];
 
     <div v-if="isView">
       <div class="form-element" v-for="forecast in weatherForecast" v-bind:key="forecast.id">
-        <p class="form-element__date">{{ new Date(forecast.dt * 1000).toLocaleDateString() }}</p>
-        <div class="form-element__titles">
-          <div v-for="title in titles" v-bind:key="title.id" class="form-element__title">
-            <h5>{{ title }}</h5>
-          </div>
-        </div>
-        <div class="form-element__datas">
-          <div class="form-element__data">
-            <h5>{{ forecast.main.temp }}</h5>
-            <h5>{{ forecast.main.humidity }}</h5>
-          </div>
-        </div>
-        <div class="form-element__metrics">
-          <div v-for="afterTitle in afterTitles" v-bind:key="afterTitle.id" class="form-element__metric">
-            <h5>{{ afterTitle }}</h5>
-          </div>
-        </div>
+        <WeatherBlock
+          :forecast="forecast"
+          :titles="titles"
+          :todayWeather="todayWeather"
+          :afterTitles="afterTitles"
+          />
       </div>
     </div>
   </div>
---
+++ src/components/WeatherBlock.vue
@@ -0,0 +1,40 @@
+<script setup lang="js">
+defineProps({
+  forecast: {
+    type: Object,
+    required: true,
+  },
+  todayWeather: {
+    type: Object,
+    required: true,
+  },
+  titles: {
+    type: Array,
+    required: true,
+  },
+  afterTitles: {
+    type: Array,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <p class="form-element__date">{{ new Date(forecast.dt * 1000).toLocaleDateString() }}</p>
+  <div class="form-element__titles">
+    <div v-for="title in titles" v-bind:key="title.id" class="form-element__title">
+      <h5>{{ title }}</h5>
+    </div>
+  </div>
+  <div class="form-element__datas">
+    <div class="form-element__data">
+      <h5>{{ forecast.main.temp }}</h5>
+      <h5>{{ forecast.main.humidity }}</h5>
+    </div>
+  </div>
+  <div class="form-element__metrics">
+    <div v-for="afterTitle in afterTitles" v-bind:key="afterTitle.id" class="form-element__metric">
+      <h5>{{ afterTitle }}</h5>
+    </div>
+  </div>
+</template>
